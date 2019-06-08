bd660f8d69ebf8343b2fc20d371b73b843887dcd
infopartha <41183333+infopartha@users.noreply.github.com>
2019-06-08 08:21:06 +0530

    Create Tamil_Parser.py
    Initial Commit

---
+++ Tamil_Parser.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  8 08:02:59 2019
+
+@author: MAGIZHINI
+"""
+
+ezhuthu = dict()
+ezhuthu['அ']={'vagai':'உயிர்','alavu':'குறில்','inam':'','uyir':'அ'}
+ezhuthu['ஆ']={'vagai':'உயிர்','alavu':'நெடில்','inam':'','uyir':'ஆ'}
+ezhuthu['இ']={'vagai':'உயிர்','alavu':'குறில்','inam':'','uyir':'இ'}
+ezhuthu['ஈ']={'vagai':'உயிர்','alavu':'நெடில்','inam':'','uyir':'ஈ'}
+ezhuthu['உ']={'vagai':'உயிர்','alavu':'குறில்','inam':'','uyir':'உ'}
+ezhuthu['ஊ']={'vagai':'உயிர்','alavu':'நெடில்','inam':'','uyir':'ஊ'}
+ezhuthu['எ']={'vagai':'உயிர்','alavu':'குறில்','inam':'','uyir':'எ'}
+ezhuthu['ஏ']={'vagai':'உயிர்','alavu':'நெடில்','inam':'','uyir':'ஏ'}
+ezhuthu['ஐ']={'vagai':'உயிர்','alavu':'நெடில்','inam':'','uyir':'ஐ'}
+ezhuthu['ஒ']={'vagai':'உயிர்','alavu':'குறில்','inam':'','uyir':'ஒ'}
+ezhuthu['ஓ']={'vagai':'உயிர்','alavu':'நெடில்','inam':'','uyir':'ஓ'}
+ezhuthu['ஔ']={'vagai':'உயிர்','alavu':'நெடில்','inam':'','uyir':'ஔ'}
+ezhuthu['க']={'vagai':'உயிர்மெய்','alavu':'குறில்','inam':'வல்லினம்','uyir':'அ'}
+ezhuthu['ங']={'vagai':'உயிர்','alavu':'குறில்','inam':'மெல்லின','uyir':'அ'}
